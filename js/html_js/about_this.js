0e45ce7ad33efac200dd7b735388e6b020b95208
Csnyi <cssnyi@icloud.com>
2024-03-14 23:14:11 +0100

    3.14.1

--- js/html_js/about_this.js
+++ js/html_js/about_this.js
@@ -16,10 +16,20 @@ var modalBody = `
         If you do, be careful! Take the proper precautions!
     </p>
     <p>
-        The project is subject to Copyleft regulation. 
-        It is built on Open Web API 1.17.
-        Data is temporarily stored in localstorage and indexedDB.
-        If someone distributes or further develops it, do with the same conditions!
+        Copyright (C) 2024 Csnyi 
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.</br>
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.</br>
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <a href="https://www.gnu.org/licenses/" target="_blank"> https://www.gnu.org/licenses/ </a>.
     </p>
     <p>
         We welcome all observations and comments: (GitHub link).
